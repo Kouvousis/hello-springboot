213df4e150f40ce5dc9953c3bafb73a5a7558e4c
Kouvousis <dexous1996@gmail.com>
2024-10-10 21:08:00 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/springstarter/controller/HelloController.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.springstarter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/coding")
+    //@RequestMapping(path = "/coding", method = RequestMethod.GET) used on older versions
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello Coding!");
+        return "index";
+    }
+
+    @GetMapping("/welcome")
+    public String sayWelcome(@RequestParam(value = "name", defaultValue = "Guest") String name, Model model) {
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+}
